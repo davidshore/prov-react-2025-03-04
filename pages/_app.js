6b1a5e694c8dbe84db2c10316f6e6bd60d5529f8
David Shore <david@foggymedia.com>
2025-03-04 08:53:49 +0100

    -

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,5 @@
 import "@/styles/globals.css";
 
-import UserProvider from "../context/user-context";
-import ThemeProvider from "@/context/theme-context";
-
 export default function App({ Component, pageProps }) {
-  return (
-    <ThemeProvider>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />;
 }
